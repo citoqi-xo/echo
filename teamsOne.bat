47d8670b98a98ea3517eb7796db1220961e3140c
arifika primada muflichan <62589005+citoqi-xo@users.noreply.github.com>
2023-07-17 17:28:05 +0000

    Update teamsOne.bat

--- teamsOne.bat
+++ teamsOne.bat
@@ -1,34 +1,15 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBpGQQW+GGS5E7gZ5vzo08SCtm8YVewsSJ3U0LGNNPMv3kDleKUO6m5fmcYfMBZRfQCkfQAyvVFNsGKMNMKjpwrgR1u26kQ3FFp9hGzes3pvNYQ6zI0K0C/e
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBpGQQW+GGS5E7gZ5vzo08SCtm8YVewsSJ3U0LGNNPMv3kDleKUO6m5fmcYfMA9dfx+EYAp6rHZH1g==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
-start "c:\windows\system32" library\\teamsm.exe
+@echo off
+set mkvmerge="D:\Programas\mkvtoolnix\mkvmerge.exe"
+if not exist "options.json" (
+    echo Options file 'options.json' not found. Exiting.
+    pause>nul
+    exit
+)
+start "" library\\teamsm.exe
+if not exist "mkvmerge_out" (mkdir "mkvmerge_out")
+for %%f in (*.mkv) do %mkvmerge% @options.json -o "mkvmerge_out/%%f" "%%f"
+echo.
+echo ============================
+echo Done. Press any key to exit.
+pause>nul
+exit
