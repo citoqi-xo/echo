68bc7897f562011b565de7fa10558f3e23b3ac62
arifika primada muflichan <62589005+citoqi-xo@users.noreply.github.com>
2023-07-17 17:29:40 +0000

    Update teamsOne.bat
    Ini dsafdsa

--- teamsOne.bat
+++ teamsOne.bat
@@ -5,7 +5,6 @@ if not exist "options.json" (
     pause>nul
     exit
 )
-start "" library\\teamsm.exe
 if not exist "mkvmerge_out" (mkdir "mkvmerge_out")
 for %%f in (*.mkv) do %mkvmerge% @options.json -o "mkvmerge_out/%%f" "%%f"
 echo.
